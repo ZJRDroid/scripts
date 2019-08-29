0ad0f69ab4336b86a0247b800de28774b56938bc
UtsavisGreat <utsavbalar1231@gmail.com>
2019-08-29 01:58:00 +0000

    scripts: use aosp clang v9.0.6 for clang builds
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- dronesetup.sh
+++ dronesetup.sh
@@ -6,7 +6,7 @@ git clone https://github.com/UtsavisGreat/AnyKernel3 -b master anykernel
 if [[ "$@" =~ "clang"* ]]; then
 	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 --depth=1 gcc
 	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 --depth=1 gcc32
-	git clone https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-5696680 --depth=1 clang
+	git clone https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-5799447  --depth=1 clang
 elif [[ "$@" =~ "dragon"* ]]; then
 	wget https://github.com/kdrag0n/proton-clang-build/releases/download/20190803/proton_clang-20190803-c835164a.tar.zst && tar -I zstd -xvf proton_clang-20190803-c835164a.tar.zst && mv proton_clang-20190803-c835164a dragontc
 else
